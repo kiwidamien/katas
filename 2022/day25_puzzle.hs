2bbdfbd815602768a880820d85a565ac24923d7f
kiwidamien <damien.martin@stitchfix.com>
2022-12-26 11:37:27 -0800

    another day in haskell

--- 2022/day25_puzzle.hs
+++ 2022/day25_puzzle.hs
@@ -11,22 +11,14 @@ digitToDec '-' = -1
 digitToDec x = ord(x) - ord('0')
 
 
---base5ToSnafu :: String -> Int
---base5ToSnafu "" = 0
---base5ToSnafu b5 = lastDigit + 5 * (base5ToDec otherDigits)
---    where reversed = reverseList b5
---          lastDigit = digitToDec $ head reversed
---          otherDigits = reverseList $ tail reversed
-
-base5ToSnafu :: String -> Int
-base5ToSnafu numString = sum $ map (convertTuple) digitAndPower
+snafuToInt :: String -> Int
+snafuToInt numString = sum $ map (convertTuple) digitAndPower
   where convertTuple (num, place) = (digitToDec num) * (5^place)
         digitAndPower = zip (reverseList numString) [0..] 
 
 
 _powersOfFive :: Int -> [Int]
-_powersOfFive num = map ((^) 5 ) $ reverseList $ takeWhile (\pow-> (div num (5^pow)) > 0) [0..]
-
+_powersOfFive num = reverseList $ takeWhile (\d-> (div num d) >0) $ map ((^) 5) [0..]
 
 decToBase5 :: Int -> [Int]
 decToBase5 num = go num (_powersOfFive num) []
@@ -37,3 +29,15 @@ decToBase5 num = go num (_powersOfFive num) []
                     divisor = div num greatPow
 
 
+intToSnafu :: Int -> String
+intToSnafu num = map (\x->snafuDigit (fst x)) preDigit
+    where snafuDigit x = cycle ['0', '1', '2', '=', '-'] !! (x+5)
+          preDigit = if (snd $ head pd) == 1 then [(1, 0)] ++ pd else pd
+          pd = take (length numAndCarry) $ scanr combine (0,0) numAndCarry
+          combine (d, c) (_, oc) = if (d+oc)>=3 then (d+oc-5, c+1) else (d+oc, c)
+          numAndCarry = map (\x -> if x>=3 then (x-5, 1::Int) else (x, 0::Int)) $ decToBase5 num	
+
+main = do
+  contents <- readFile "day25_input.txt"
+  let result = intToSnafu <$> sum $ map (snafuToInt) $ lines $ contents
+  return result
